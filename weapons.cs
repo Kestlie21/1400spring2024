2ad243b309dbf03add65bb2f2fece812136cb6f8
Kestlie21 <156541869+Kestlie21@users.noreply.github.com>
2024-01-24 20:17:56 -0700

    Update weapons.cs

--- weapons.cs
+++ weapons.cs
@@ -1,18 +1,18 @@
-	public PowerUp weaponPowerUp;
+	public PowerUp weaponPowerUp;#name
 	public  void Main()
 	{
 		bow = new Weapon();
 		weaponPowerUp = new PowerUp();
-		bow.ammoCount+=20;
-		bow.fireSpell+= weaponPowerUp.powerLevel;
-		Console.WriteLine(bow.weaponNames + " has an ammo count of " + bow.ammoCount );
-		Console.WriteLine("The spell power has now increased " + bow.fireSpell + " times. ");
+		bow.ammoCount+=20; #adds 20 more to ammo
+		bow.fireSpell+= weaponPowerUp.powerLevel;#adds more power to starting amount
+		Console.WriteLine(bow.weaponNames + " has an ammo count of " + bow.ammoCount );#prints the amount and string
+		Console.WriteLine("The spell power has now increased " + bow.fireSpell + " times. ");#same printing 
 	}
 }
 public class Weapon{
-	public int ammoCount = 20;
+	public int ammoCount = 20;#starting amount of ammo
 	public string weaponNames = "bow";
-	public float fireSpell = 12.5f;
+	public float fireSpell = 12.5f;#starting amount of fire spell level
 }	
 public class PowerUp{
 	public float powerLevel = 3.5f;
